58012c9463e0d054b35927300ed4f7189d807b8b
pkale <pkale@ncsu.edu>
2024-05-31 19:07:09 -0400

    [Javadoc] Main Class

--- SnakeGame.java
+++ SnakeGame.java
@@ -1,4 +1,15 @@
+/**
+ * Snake Game Application is your traditional style Snake game where the player controls
+ * a snake to eat food, grow longer, and avoid running into walls or the snake's own body.
+ * The game is initiated from the main method, which creates a new game frame.
+ *
+ * @author Pranav Kale
+ */
 public class SnakeGame {
+    /**
+     * Main method for beginning program
+     * @param args arguments
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
